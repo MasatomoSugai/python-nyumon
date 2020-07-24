79489adc2e9b4310e1f15692248d0232aa13611b
Masatomo Sugai <tech.masatomo.s@gmail.com>
2020-07-24 11:52:44 +0900

    2020/07/24

--- chap5.py
+++ chap5.py
@@ -171,14 +171,193 @@ while文
 continue
 """
 # 重複しない値が１０個入ったリストを作る
-from random import randint
-numbers = []
-while len(numbers)<10:
-    n = randint(0,100)
-    print("{}が出ました！".format(n))
-    if n in numbers :
-        continue  #すでに含まれている値は追加せずに処理をスキップして次の値にいく
-    numbers.append(n)
-    print(numbers)
+# from random import randint
+# numbers = []
+# while len(numbers)<10:
+#     n = randint(0,100)
+#     print("{}が出ました！".format(n))
+#     if n in numbers :
+#         continue  #すでに含まれている値は追加せずに処理をスキップして次の値にいく
+#     numbers.append(n)
+#     print(numbers)
 
-print("最終的にこれっす",numbers)
+# print("最終的にこれっす",numbers)
+
+"""
+while ~ else
+"""
+# from random import randint
+# numbers = []
+# while len(numbers)<10:
+#     n = randint(-10,90)
+#     if n<0:
+#         print(f"{n}が出たので中断されました")
+#         break
+#     if n in numbers :
+#         continue
+#     numbers.append(n)
+# else:
+#     print(numbers)
+
+
+"""
+section5-3
+for文　処理を繰り返す
+"""
+
+"""
+n回繰り返すfor文.０から始まってn回繰り返す。最後の値はn-1
+for i in range(n):
+"""
+
+# for i in range(10):
+#     print(i)
+
+#range(10)は,iが繰り返しを数えるカウンタのように見えますが、0~9のシーケンスから値を１つずつ取り出してiに代入しています。
+#range(開始値,終了値,ステップ)
+# for i in range(5,10):
+#     print(i)
+
+# forのネスティング
+# for i in range(3):
+#     print("i =", i)
+#     for j in range(7,9):
+#         print(" ", "j =",j)
+
+# 4行３列の点の座標
+# 垂直方向３、水平方向２の感覚で並ぶ4行３列の点の座標を求める
+# for i in range(4):
+#     print()  #各行の改行
+#     for j in range(3):
+#         x = j*2
+#         y = i*3
+#         print(f"({x}, {y})", end="") #end=""で改行なし？
+# print()  #最後の改行
+
+# 繰り返しを中断んして終了するbreak
+# numlist = [3, 4.4, 6,"x",1,7]
+# sum = 0
+# for num in numlist:   #変数numとして、オブジェクトnumlistから値を取り出す
+#     if not isinstance(num, (int, float)): #isinstanceで数値(int, float)の判定
+#         print(num, "数値ではありません。")
+#         break
+#     sum += num
+#     print(num, "/", sum)
+
+# ネスティングのbreak
+# for i in range(4):
+#     for j in range(4):
+#         if i<j:
+#             print("." * j)
+#             break
+#         print(f"i={i}, j={j}")
+
+#continueでスキップ
+# numlist = [3, 4.4, 6,"x",1,7]
+# sum = 0
+# for num in numlist:   #変数numとして、オブジェクトnumlistから値を取り出す
+#     if not isinstance(num, (int, float)): #isinstanceで数値(int, float)の判定
+#         print(num, "数値ではありません。")
+#         continue
+#     sum += num
+#     print(num, "/", sum)
+
+# ネスティングの中でcontinueを使う
+# for i in range(4):
+#     for j in range(4):
+#         if i<j:
+#             print("con " * j)
+#             continue
+#         print(f"i={i}, j={j}")
+
+"""
+繰り返した後で実行するfor in ~ else
+"""
+# numlist = [3, 4.4, 6,99,1,7]
+# sum = 0
+# for num in numlist:   #変数numとして、オブジェクトnumlistから値を取り出す
+#     if not isinstance(num, (int, float)): #isinstanceで数値(int, float)の判定
+#         print(num, "数値ではない値が含まれていました。")
+#         break
+#     sum += num
+# else:  #breakされなかったときに実行される
+#     print(num, "/", sum)
+
+
+"""
+section5-4
+try文　例外処理
+"""
+"""
+try:
+  例外が発生する可能性がある処理
+except:
+  例外を受けて実行する処理
+finally:
+  try文を抜ける前に実行する処理
+"""
+
+# while True :
+#     num = input("何個ですか？(qで終了)")
+#     if num == "q":
+#         print("終了しました。")
+#         break
+#     try:
+#         price = 12 * int(num) #numのままだと文字列なので、int()で整数に変換
+#         print("金額：",price)
+#     except :
+#         print("エラーです。正しい数値を入れてください。")
+
+
+# num = input("整数を入れてね")
+# try:
+#     value =120 / int(num)
+#     print(value)
+# except:
+#     print("エラーになりました。")
+# finally:
+#     print("計算終わり。") #エラーが発生してもしなくても最後に実行される
+
+"""
+関数内で値をreturnする場合、そのtry文にfinallyブロックがあるなら、
+finallyブロックで値をreturnする必要がある
+"""
+
+"""
+NameError:初期化されていない変数を使った
+ZeroDivisionError:ゼロの割り算を行った
+ValueError:方を変換できなかった
+IndexError:空のリストから値を取り出そうとした
+FileNotFoundError:指定したファイルが見つからなかった
+
+"""
+
+# sum = 7600
+# while True:
+#     num = input("何人ですか？（qで終了）")
+#     if num == "q":
+#         print("終了しました。")
+#         break
+#     try:
+#         price = round(sum / int(num))
+#         if price <0:
+#             continue
+#         print("一人当たりの金額", price)
+#     except ValueError:
+#         print("数値を入れてください。")
+#     except ZeroDivisionError:
+#         print("0以外の数値を入力してください。")
+
+"""
+try:
+  例外が発生する可能性がある処理
+except 例外１:
+  例外１に対応する例外処理
+except 例外２：
+  例外２に対応する例外処理
+else:
+  例外が発生しなかったときに実行する処理
+finally:
+  try文を抜ける前に必ず実行する処理
+  
+"""
